8d0d839f9ad0e81966516cacbe8ef6c8f84574e4
LucasLemos1007 <lucasmagicfx@hotmail.com>
2020-04-25 14:07:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let tableBody = document.querySelector('.t-body');
 
 fetch('http://localhost:3000/materiais').then(async (response) => {
   const data = await response.json()
-  console.log(data)
-
+  
+  //console.log(data)
   resolvendoPublicacoes(data)
 }).catch(err => {
   console.log(err)
