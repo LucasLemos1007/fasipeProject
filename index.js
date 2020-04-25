cce992f19eacc8b6f4d703604581693dcecddc14
Gruposimulado <48774829+Gruposimulado@users.noreply.github.com>
2020-04-25 14:05:15 -0400

    primeiro commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let tableBody = document.querySelector('.t-body');
+
+fetch('http://localhost:3000/materiais').then(async (response) => {
+  const data = await response.json()
+  console.log(data)
+
+  resolvendoPublicacoes(data)
+}).catch(err => {
+  console.log(err)
+})
+
+function resolvendoPublicacoes(data) {
+  data.map(s => {
+    return tableBody.innerHTML += `<tr><td>${s.nome}</td><td>${s.descricao}</td><td>${s.duracao}</td></t`
+  })
+}
+
+// Otimizado... eu acho
